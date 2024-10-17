6eb3ec8c4efe274e6d2015679fa87d047db46856
prashant2231109 <prashant2231109@akgec.ac.in>
2024-10-17 21:48:26 +0530

    model

--- deploymodel/views.py
+++ deploymodel/views.py
@@ -2,16 +2,29 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import joblib 
 
-
+import os
 
 def home(request):
     return render(request,"home.html")
 
 def result(request):
+    model_path = os.path.join('static', 'CRM.pkl')
+    joblib.dump(cls, 'CRM.pkl', compress=3)
+
+    cls = joblib.load(model_path)
+# def result():
+#     model_path = os.path.join('static', 'CRM.pkl')
+#     try:
+#         return joblib.load(model_path)
+#     except Exception as e:
+#         print(f"Error loading model: {e}")
+#         return None
+
+# # Preload the model
+# model = result()
+
     
-    cls=joblib.load("CRM.pkl")
-    
-    lis=[]
+    lis = []
     
     lis.append(request.GET['Temparature'])
     lis.append(request.GET['Humidity '])
@@ -23,8 +36,6 @@ def result(request):
     lis.append(request.GET['Phosphorous '])
     print(lis)
     
-    
-    ans=cls.predict([lis])    
-    
+    ans = cls.predict([lis])    
     
     return render(request,"result.html",{'ans':ans})
